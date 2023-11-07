61bc8c1dc140accad15595a863e678b81dc10338
MSI <johndoe@example.com>
2023-11-07 21:14:24 +0700

    Update Electron

--- server/uninstall.js
+++ server/uninstall.js
@@ -3,7 +3,7 @@ var Service = require('node-windows').Service;
 const svc = new Service({
     name: "ckd-server",
     description: "ok",
-    script: "D:\\System\\CKD-System\\server\\src\\index.ts"
+    script: "D:\\System\\CKD-System\\server\\dist\\index.js"
 })
 
 // Listen for the "uninstall" event so we know when it's done.
