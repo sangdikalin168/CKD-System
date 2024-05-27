f7251c4e923f6ca3aec309e6bc1a56b271d9bb29
Kalin <kalin@gmail.com>
2024-05-27 15:13:42 +0700

    Add Start System Bat File

---
+++ start_system.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d "D:\CKD-System\client"
+Title "Your needed title should be typed here without quote"
+echo Starting React app...
+start /min yarn start
+
+@echo off
+cd /d "D:\CKD-System\server"
+Title "Your needed title should be typed here without quote"
+echo Starting React app...
+start /min yarn start
